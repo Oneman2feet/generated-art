16b4a2080e87082c6671cee1df272759ca2cd4a0
Zachary Zimmerman <Oneman2feet@gmail.com>
2014-12-18 17:52:41 -0500

    random triangles

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-var s = new Snap("svg");
-var bigCircle = s.circle(150, 150, 100);
-bigCircle.animate({cx:1000}, 5000);
+var paper = new Snap("svg");
+
+function makeTriangle(x, y, size, rotation, color) {
+    var triangle = paper.polygon(-0.5, -0.5, -0.5, 0.5, 0.5, 0.5).attr({
+        fill: color
+    });
+
+    var matrix = new Snap.Matrix();
+    matrix.translate(x,y);
+    matrix.rotate(rotation);
+    matrix.scale(size);
+
+    triangle.transform(matrix);
+}
+
+function makeRandomTriangle() {
+    var size = Math.random() * 500 + 20;
+    var color = randomColor();
+    var rotation = Math.floor(Math.random()*4) * 90;
+    var x = Math.random()*1500;
+    var y = Math.random()*800;
+
+    makeTriangle(x, y, size, rotation, color);
+}
+
+for (var i=0; i<100; i++) {
+    makeRandomTriangle();
+}
