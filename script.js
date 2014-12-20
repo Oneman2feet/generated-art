a89c8b61bf2478458321b700e44ce2da3069e883
Zachary Zimmerman <Oneman2feet@gmail.com>
2014-12-20 14:04:35 -0500

    color picker

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var paper = new Snap("svg");
 var width  = window.screen.width;
 var height = window.screen.height;
 
+/*
 function makeTriangle(x, y, size, rotation, color) {
     var triangle = paper.polygon(-0.5, -0.5, -0.5, 0.5, 0.5, 0.5).attr({
         fill: color
@@ -35,6 +36,7 @@ var color = Math.random() * 256;
 for (var i=0; i<200; i++) {
     makeRandomTriangle(color);
 }
+*/
 
 Snap.load("/assets/glyph.svg", function (shape) {
     shape = shape.select("g").attr('id','glyph');
@@ -51,6 +53,37 @@ function transformShape(shape, x, y, cx, cy, angle, scalex, scaley) {
     shape.transform(matrix);
 }
 
+var colors = [
+    {color:'#81eaa9', weight:1},
+    {color:'#60d6be', weight:2},
+    {color:'#d0f282', weight:2},
+    {color:'#4ae2d6', weight:2},
+    {color:'#99fcc6', weight:1}
+];
+
+var sum = 0;
+var cdf = colors.reduce(function(acc,el){
+    sum = (acc.length===0 ? 0 : acc[acc.length-1]) + el.weight;
+    acc.push(sum);
+    return acc;
+},[]);
+
+function binsearch(value, array) {
+    var recbin = function(start,end) {
+        if (start===end) return start;
+        var test = Math.floor((end+start)/2);
+        if (array[test]===value) return test;
+        if (array[test]<value) return recbin(test+1,end);
+        return recbin(start,test);
+    };
+    return recbin(0,array.length-1);
+}
+
+function pickColor() {
+    var i = Math.random()*sum;
+    return colors[binsearch(i,cdf)].color;
+}
+
 function colorShape(shape) {
     var groups = shape.selectAll("g").items;
     for (i in groups) {
@@ -58,7 +91,7 @@ function colorShape(shape) {
         setGroup(group, {
             'stroke': 'black',
             'stroke-width': '1px',
-            'fill': randomColor({hue:'orange'})
+            'fill': pickColor()
         });
     }
 }
