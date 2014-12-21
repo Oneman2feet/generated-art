7ffd82c1de93815b525a47874fc762f086af4f07
Zachary Zimmerman <Oneman2feet@gmail.com>
2014-12-21 15:52:12 -0500

    buttons and slight refactor

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ var paper = new Snap("svg");
 var buttons = document.body.getElementsByTagName("button");
 for (var i=0; i<buttons.length; ++i) {
     buttons[i].addEventListener("click", function(e){
-        paper.clear();
-        var script = document.createElement('script');
-        script.setAttribute('src','/compositions/'+e.target.name+'.js');
-        document.head.appendChild(script);
+        var composition = e.target.name;
+        loadComposition(composition);
     });
+}
+
+// load the first composition
+var defaultComposition = 'mayan';
+var first = window.location.hash.slice(1);
+if (first==='') first = defaultComposition;
+loadComposition(first);
+
+// display the given composition on the page
+// TODO: don't fetch again on second time
+function loadComposition(name) {
+    paper.clear();
+    window.location.hash = name;
+    var script = document.createElement('script');
+    script.setAttribute('src','/compositions/'+name+'.js');
+    document.head.appendChild(script);
 }
